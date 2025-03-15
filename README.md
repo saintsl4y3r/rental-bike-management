87b67bcade67ad43d2cde9704c25fc6674a86296
Cao Sỹ Siêu <148168692+saintsl4y3r@users.noreply.github.com>
2025-03-15 02:56:20 +0000

    rebuild

--- README.md
+++
@@ -1,8 +0,0 @@
-# Quản lý dịch vụ cho thuê xe máy
-
-## Cách chạy trên GitHub Codespaces
-
-### 1. Khởi động Backend
-```sh
-cd backend
-node server.js
--- backend/models/bike.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const BikeSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    quantity: Number,
-});
-
-module.exports = mongoose.model("Bike", BikeSchema);
